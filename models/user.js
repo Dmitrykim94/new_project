211f9a2ceeddd0a9b1085f4e76880460e04a1c0d
Katrin _Alimova <alimova.katrin8@gmail.com>
2019-05-23 15:53:40 +0300

    Likes Trash

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     tagArray: Array,
+    preferences: Array,
 })
 
 module.exports = mongoose.model('User', userSchema);
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const likeIncrement = document.getElementById('likeIncrement');
-    
-    likeIncrement.addEventListener('submit', async (e) => {
-        e.preventDefault();
+    const PostCard = document.querySelectorAll('.card-body');
+    // const PostCard = document.getElementsByClassName('container mt-4');
 
-        const likeForOnePost = document.getElementById('like').value;
-       
-        // continue here
+    // console.log(PostCard)
+    PostCard.forEach(function (item) {
+        item.addEventListener('click', async (e) => {
+            e.preventDefault();
+
+            const likeForOnePost = item.getElementsByClassName('like');
+            const valueOfLikes = item.children[3];
+            const likeCorrect=valueOfLikes.getAttribute('value');
+            const postName = item.children[0];
+           const postNameCorrect = postName.getAttribute('value');
+            // console.log(postNameCorrect)
+            let res = await fetch('/main', {
+                method: 'PUT',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    like: likeCorrect,
+                    postName: postNameCorrect
+                })
+            });
+            let updatedPost = await res.json()
+            likeForOnePost.innerText = `Likes: ${updatedPost.likeUpdated}`
+        });
+    });
 });
