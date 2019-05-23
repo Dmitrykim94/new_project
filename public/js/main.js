fa845380ec11b68de8a2c8531e255cae325e5f8d
Dmitrykim94 <dmitrykim-l@mail.ru>
2019-05-23 17:00:14 +0300

    updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const PostCard = document.querySelectorAll('.card-body');
-    // const PostCard = document.getElementsByClassName('container mt-4');
-
-    // console.log(PostCard)
+    
     PostCard.forEach(function (item) {
         item.addEventListener('click', async (e) => {
             e.preventDefault();
-
+            
             const likeForOnePost = item.getElementsByClassName('like');
             const valueOfLikes = item.children[3];
             const likeCorrect=valueOfLikes.getAttribute('value');
             const postName = item.children[0];
-           const postNameCorrect = postName.getAttribute('value');
+            const postNameCorrect = postName.getAttribute('value');
             // console.log(postNameCorrect)
             let res = await fetch('/main', {
                 method: 'PUT',
@@ -29,11 +27,10 @@ document.addEventListener("DOMContentLoaded", function () {
             likeForOnePost.innerText = `Likes: ${updatedPost.likeUpdated}`
         });
     });
-  
-  const likeIncrement = document.getElementById('likeIncrement');
+    
     const form = document.querySelector('form[class="form-inline my-2 my-lg-0"]');
     const searchInput = document.querySelector('input[class="form-control mr-sm-2"]');
- 
+    
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
         window.location = `/filter?tag=${searchInput.value}`
@@ -42,5 +39,4 @@ document.addEventListener("DOMContentLoaded", function () {
   
 });
 
-})
 
