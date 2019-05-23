981ea7b7c74f3192ee1f7bcb655b5d6789e7f42a
Katrin _Alimova <alimova.katrin8@gmail.com>
2019-05-23 21:30:05 +0300

    Fixed likes for filtration

--- public/js/filter.js
+++ public/js/filter.js
@@ -3,8 +3,41 @@ document.addEventListener("DOMContentLoaded", function () {
     const searchInput = document.querySelector('input[class="form-control mr-sm-2"]');
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
+        // console.log('gjfjhkgfkhkfgkjfgj')
         if (!searchInput.value)
             return;
         window.location = `/filter?tag=${searchInput.value}`
-    })
+    });
+
+    
+    const PostCard = document.querySelectorAll('div[class="col-auto mb-3"]');
+
+    PostCard.forEach(function (item) {
+        item.addEventListener('click', async (e) => {
+            e.preventDefault();
+          
+            const valueOfLikes = item.querySelector("span[id='like']");
+            const likeCorrect=valueOfLikes.getAttribute('value');
+            const postName = item.querySelector("p[class='postName']");
+            const postNameCorrect=postName.getAttribute('value');
+            
+            let res = await fetch(`/filter?tag=${searchInput.value}`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    like: likeCorrect,
+                    postName: postNameCorrect
+                })
+            });
+            let updatedPost = await res.json()
+            console.log(valueOfLikes)
+            valueOfLikes.innerText = `Likes: ${updatedPost.likeUpdated}`
+            valueOfLikes.setAttribute('value',  updatedPost.likeUpdated)
+          
+        });
+    });
+    
 })
